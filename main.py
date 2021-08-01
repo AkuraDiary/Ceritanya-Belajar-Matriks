0e19038185464928b241aa42d218692bdcfc3dc3
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-08-01 12:48:59 +0700

    masih blm bisa nge transpose transposed matriks

--- main.py
+++ main.py
@@ -7,20 +7,14 @@ Data = [
   [3, 6, -12, 16, 20],
   [-2, 7, 4, 6, -3]
 ] 
-Data_2 = [
-  [3, -2], 
-  [6, 7], 
-  [-12, 4], 
-  [16, 6], 
-  [20, -3]
-]
+
 A = M.matriks(val = Data) #objek matriks
 print("matriks : ", A.Array)
 print("Ordo : ", A.ordo())
 print("diagonal [main][secondary]: ", A.diagonal())
-B = A.transpose()
-print(B)
-C = M.matriks(val = Data_2)
-A_Transpose = M.matriks(B)
-print("matriks transpose : ", A_Transpose.Array)
+B = M.matriks(A.transpose())
+print("matriks transpose : ", B.Array)
+# masih eror
+C = M.matriks(B.transpose())
+print(C.Array) 
 
